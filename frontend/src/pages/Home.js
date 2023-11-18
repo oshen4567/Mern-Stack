5317b71e90cba4d83645cfedbd7f1330bd559fba
oshen4567 <COHDSE221F-013@student.nibm.lk>
2023-11-18 20:48:11 +0530

    3rd commit
    Finished building the whole application using mern stack

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,34 @@
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
+//components
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutsForm'
+
 const Home = () =>{
+   
+    const {workouts, dispatch} = useWorkoutsContext()
+
+    useEffect(()=>{
+        const fetchWorkouts = async () =>{
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if(response.ok){
+                dispatch({type: 'SET_WORKOUTS', payload: json})
+            }
+        }
+        fetchWorkouts()
+    }, [dispatch])
+
     return(
         <div className="Home">
-            <h2>Home</h2>
+            <div className="workouts">
+            {workouts && workouts.map((workout)=> (
+                <WorkoutDetails key={workout._id} workout={workout}/>
+            ))}
+            </div>
+            <WorkoutForm />
         </div>
     )
 }
